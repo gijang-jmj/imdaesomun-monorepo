9cc41368eb296db6bc74fe95612dd8398c4d1a92
jmj <wnalsals1127@gmail.com>
2025-08-10 00:15:30 +0900

    task: add og image

--- apps/web-next/src/constants/seo.ts
+++ apps/web-next/src/constants/seo.ts
@@ -5,10 +5,10 @@ export const baseOpenGraph: Metadata['openGraph'] = {
   siteName: '임대소문',
   title: '임대소문',
   description: '공공임대 정보 한눈에 보기',
-  url: 'https://imdaesomun.web.app',
+  url: 'https://imdaesomun.vercel.app',
   images: [
     {
-      url: 'https://imdaesomun.web.app/og.png',
+      url: 'https://imdaesomun.vercel.app/og.png',
       width: 1200,
       height: 630,
     },
