ada6eb94d36628ae41fb23eb1193d4c51d031f19
jmj <wnalsals1127@gmail.com>
2025-08-10 00:26:08 +0900

    task: add og url in ssr

--- apps/web-next/src/app/notice/[noticeId]/page.tsx
+++ apps/web-next/src/app/notice/[noticeId]/page.tsx
@@ -22,6 +22,7 @@ export async function generateMetadata({
         '서울, 경기, 인천, 부산 등 SH·GH·IH·BMC 공사의 공공임대주택 정보를 한곳에 모았습니다. 임대소문에서 주택청약과 임대 공고를 빠르게 확인하세요.',
       openGraph: {
         ...baseOpenGraph,
+        url: `https://imdaesomun.vercel.app/notice/${noticeId}`,
         type: 'article',
         title: `임대소문 - ${getNoticeCorporationTypeKor(notice.corporation)}`,
         description: notice.title,
